1e56bed276dfcb7554dc6b04b959622b28a79869
Photi31 <photiniya@gmail.com>
2023-07-21 17:50:06 +0300

    start

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ["@it-incubator/eslint-config", "plugin:storybook/recommended"],
+  rules: {
+    "no-console": [
+      "warn",
+      {
+        allow: ["warn", "error"],
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["react"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
